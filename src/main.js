bc1d5e02014a31e86b2e221b275b4e13bb8c1263
Spike <spike@SpikedeMacBook-Pro.local>
2019-09-10 22:44:03 +0800

    增加elementui和vuetify

--- src/main.js
+++ src/main.js
@@ -4,14 +4,17 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import axios from 'axios'
-
 import './assets/less/site.less'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
 
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
 Vue.prototype.HOST = '/api'
+
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
