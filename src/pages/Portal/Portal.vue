48ae763d501f1cf72eb9c981ce05f91e8ba00e40
ShroXd <shroxdf@gmail.com>
2019-09-11 19:02:28 +0800

    init protal 导航栏

--- src/pages/Portal/Portal.vue
+++ src/pages/Portal/Portal.vue
@@ -1,18 +1,17 @@
 <template>
-  <div>
-    <div class="login">
-      <v-form>
-        <v-container class="container">
-          <v-text-field label="用户名" required>
-          </v-text-field>
-          <v-text-field label="密码" required>
-          </v-text-field>
-          <v-text-field label="E-mail" required></v-text-field>
-          <v-btn class="entry-btn" depressed color="primary">登录</v-btn>
-          <v-btn class="entry-btn" depressed color="primary">注册</v-btn>
-        </v-container>
-      </v-form>
-    </div>
+  <div class="portal">
+    <v-toolbar>
+      <v-toolbar-title>Bebop</v-toolbar-title>
+      <div class="flex-grow-1"></div>
+      <v-toolbar-items>
+        <v-btn text>首页</v-btn>
+        <v-btn text>归档</v-btn>
+        <v-btn text>数据</v-btn>
+      </v-toolbar-items>
+      <v-btn icon>
+        <v-icon>mdi-export-variant</v-icon>
+      </v-btn>
+    </v-toolbar>
   </div>
 </template>
 
@@ -28,29 +27,8 @@ export default {
 <style lang="less" scoped>
   @import "../../assets/less/color.less";
 
-  .login {
-    height: 100vh;
-    width: 100vw;
-    user-select: none;
-    background: url("../../assets/img/entry_background.jpg") no-repeat;
-    background-size: cover;
-    background-position: center;
-
-    .container {
-      background-color: @_sys-white;
-      border-radius: 10px;
-      box-shadow: 1px 1px 3px @_sys-mid-gray;
-      padding: 1.2rem;
-      width: 20vw;
-      position: absolute;
-      top: 15vh;
-      left: 20vw;
-
-      .entry-btn {
-        float: right;
-        margin-left: 0.6rem;
-      }
-    }
+  .portal {
+    background-color: @_sys-super-light-gray;
   }
 
 </style>
