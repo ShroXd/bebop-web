7b7cc8238b9680781204355661940e45d062462b
ShroXd <shroxdf@gmail.com>
2020-01-06 10:34:07 +0800

    引入fontawesome

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import '@mdi/font/css/materialdesignicons.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 Vue.use(Vuetify)
 
 export default new Vuetify({
