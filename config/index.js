e9ec83c0729ed2a50e2bf0cf45c6cf2d8a40c047
ShroXd <shroxdf@gmail.com>
2020-01-09 12:26:40 +0800

    修改host

--- config/index.js
+++ config/index.js
@@ -12,7 +12,8 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://118.31.33.69:3000',
+        //target: 'http://118.31.33.69:3000',
+        target: 'http://localhost:3000',
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
@@ -21,7 +22,7 @@ module.exports = {
     },
 
     // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
+    host: '0.0.0.0', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
