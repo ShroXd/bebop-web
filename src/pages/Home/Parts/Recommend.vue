786fbe379644dc90a563518510ef89647779d07c
ShroXd <shroxdf@gmail.com>
2020-04-26 17:16:51 +0800

    add 本周推荐

---
+++ src/pages/Home/Parts/Recommend.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="recommend"></div>
+</template>
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+@import '../../../assets/less/pageless/index/recommend.less';
+</style>
