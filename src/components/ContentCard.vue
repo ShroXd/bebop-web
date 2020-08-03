c88d0e4de012faf3bbc4fc31ad03b948931b7e9e
ShroXd <shroxdf@gmail.com>
2020-08-03 11:23:11 +0800

    feat (content-card): 增加内容卡片组件

---
+++ src/components/ContentCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="content-card">
+    <div class="content-card__title">
+      {{ name }}
+    </div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ContentCard',
+  props: {
+    name: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  .content-card {
+    border: 1px solid #d8d8d8;
+    border-top: 3px solid #1966c0;
+    /*border-radius: 6px;*/
+    background-color: white;
+  }
+
+  .content-card__title {
+    border-bottom: 1px solid #d8d8d8;
+    height: 2.6rem;
+    padding-left: 1.4rem;
+    background-color: #f7f7f7;
+    font-size: 1.1rem;
+    line-height: 2.6rem;
+    color: #1966c0;
+  }
+</style>
