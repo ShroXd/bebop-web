894e03c2812855ee75406e6a53cb3c2193daa3e6
ShroXd <shroxdf@gmail.com>
2020-08-04 17:59:35 +0800

    refactor (index): 重构了 index 的样式

--- src/pages/Home/Index.vue
+++ src/pages/Home/Index.vue
@@ -1,9 +1,27 @@
 <template>
   <div class="index-container">
     <div class="portal">
-      <ContentCard name="小说列表">
+      <v-card outlined>
+        <v-list-item three-line>
+          <v-list-item-content>
+<!--            <div class="overline mb-4">OVERLINE</div>-->
+            <v-list-item-title class="headline mb-1">小说清单</v-list-item-title>
+            <v-list-item-subtitle>是黑猫，不是臭鼬。请停止你们的迫害行为（认真）</v-list-item-subtitle>
+          </v-list-item-content>
+
+          <v-list-item-avatar
+            tile
+            size="120"
+            color="grey">
+            <v-img class="novel-img"
+                   height="140"
+                   width="100"
+                   src="https://bebopfzj.oss-cn-hangzhou.aliyuncs.com/uPic/2020-04-26-nUvAU1.jpg"></v-img>
+          </v-list-item-avatar>
+        </v-list-item>
+        <v-divider></v-divider>
         <light-novel></light-novel>
-      </ContentCard>
+      </v-card>
     </div>
   </div>
 
